e3c420a2d7a56dd084183b52942aed423f6e6f9e
Dumitru Marian <mariantdm@icloud.com>
2022-05-25 17:00:58 +0100

    added button to change the size

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 "use strict";
+
+//add a button
+const button = document.createElement("button");
+
+// add class to the button
+button.classList.add("button");
+
+button.textContent = "Change the size";
+//style the button
+button.setAttribute(
+  "style",
+  "width: 20px, height: 20px, background-color: grey; border: 2px solid black; border-radius: 2%; margin-top: 0; position:absolute; top: 15px;"
+);
+document.body.appendChild(button);
+
+//the button sends the user a pop up asking for the number of squares - max input should be 100
+const changeSize = function (size) {
+  size = prompt("Chose a size between 1 - 100");
+  if (size <= 100) {
+    for (let i = 0; i < size; i++) {
+      //create divs
+      const div = document.createElement("div");
+      //style the divs
+      div.setAttribute(
+        "style",
+        "background-color: red; margin: 1px; display: inline-block; width: 16px; height: 16px;"
+      );
+      //add the divs to the container.
+      container.appendChild(div);
+
+      //change the color when mouse over divs.
+      const changeColor = () => {
+        div.style.backgroundColor = "grey";
+      };
+      div.addEventListener("mouseover", changeColor);
+    }
+  }
+};
+button.addEventListener("click", changeSize);
+//once entered, the existing grid should be removed and a new grid should be generated in the same total space as before.
 //create the container
 const container = document.querySelector("#container");
 // style the container.
 container.setAttribute(
   "style",
-  "margin-top: 200px;background-color: blue; margin-left: 200px; width: 300px; height: 300px;"
+  "margin-top: 200px; margin-left: 200px; width: 195px; height: 220px;"
 );
-
-for (let i = 0; i < 195; i++) {
-  //create divs
-  const div = document.createElement("div");
-  //style the divs
-  div.setAttribute(
-    "style",
-    "background-color: red; padding: 1px;margin: 1px; display: inline-block; width: 16px; height: 16px;"
-  );
-  //add the divs to the container.
-  container.appendChild(div);
-
-  //change the color when mouse over divs.
-  const changeColor = () => {
-    div.style.backgroundColor = "grey";
-  };
-  div.addEventListener("mouseover", changeColor);
-}
-//create button
-//add functions to the button.
